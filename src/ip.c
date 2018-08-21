4265cbd2ae1163b7e1e409ebc53de8e8cd1176ee
Kawaharasouta <kawahara6514@gmail.com>
2018-08-21 14:59:42 +0900

    ip init route table

--- src/ip.c
+++ src/ip.c
@@ -3,6 +3,55 @@
 #include<string.h>
 #include<stdint.h>
 
+#include"include/ethernet.h"
+
+#define ROUTE_TABLE_SIZE 2;
+
 uint32_t get_ip_addr(struct ether_port *port) {
 	return 0x0a000005;
+}
+
+struct route_node {
+	uint8_t used;
+	uint32_t addr;
+	uint32_t mask;
+	uint32_t next;
+}
+
+struct ip {
+	uint32_t addr;
+	uint32_t mask;
+	uint32_t network;
+	uint32_t broadcast;
+	struct route_node route_table[ROUTE_TABLE_SIZE];
+} ip;
+
+void 
+route_table_init() {
+	struct route_node *route;
+	struct route_node *fin = ip.route_table + ROUTE_TABLE_SIZE;
+
+	for (route = ip.route_table; route != fin; route++) {
+		route->used = 0;
+		route->addr = 0;
+		route->mask = 0;
+		route->next = 0;
+	}
+	return;
+}
+
+int route_table_add(const uint32_t addr, const uint32_t mask, const uint32_t next) {
+	struct route_node *route
+	struct route_node *fin = ip.route_table +  ROUTE_TABLE_SIZE;
+
+	for (route = ip.route_table; route != fin; route++) {
+		if (!route->used) {
+			route->used = 1;
+			route->addr = addr;
+			route->mask = mask;
+			route->next = next;
+			return 0;
+		}
+	}
+	return -1;
 }
